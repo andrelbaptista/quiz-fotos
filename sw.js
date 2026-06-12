15e031f337843be3adbcd1271e77c1c8286a7cfc
Andre Baptista <122994174+andrelbaptista@users.noreply.github.com>
2026-06-12 13:47:20 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,27 @@
-var CACHE = 'quiz-fotos-v1';
-var FILES = ['/', '/index.html', '/manifest.json', '/icons/icon-192.png', '/icons/icon-512.png'];
+// Quiz de Fotos - Service Worker
+const CACHE_NAME = 'quiz-fotos-v5';
+const ASSETS = [
+  './',
+  './index.html'
+];
 
 self.addEventListener('install', function(e) {
-  e.waitUntil(caches.open(CACHE).then(function(c) { return c.addAll(FILES); }));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(ASSETS).catch(function() {});
+    })
+  );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', function(e) {
-  e.waitUntil(caches.keys().then(function(keys) {
-    return Promise.all(keys.filter(function(k){ return k !== CACHE; }).map(function(k){ return caches.delete(k); }));
-  }));
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.filter(function(k) { return k !== CACHE_NAME; })
+            .map(function(k) { return caches.delete(k); })
+      );
+    })
+  );
   self.clients.claim();
-});
-
-self.addEventListener('fetch', function(e) {
-  e.respondWith(caches.match(e.request).then(function(r){ return r || fetch(e.request); }));
 });
